305e45e614727a45562935d6b61ce3f1fedae540
RomanShalak <romansalakdev@gmail.com>
2023-02-13 21:41:30 +0100

    http login and register comment

--- app/user.js
+++ app/user.js
@@ -16,104 +16,104 @@ router.get("/list", async (req, res) => {
   }
 });
 
-router.post("/login", async (req, res) => {
-  let info = req.body;
+// router.post("/login", async (req, res) => {
+//   let info = req.body;
 
-  let rows = await database.select("users", {
-    email: info.email,
-  });
+//   let rows = await database.select("users", {
+//     email: info.email,
+//   });
 
-  if (rows.data.length > 0) {
-    rows = await database.select("users", {
-      email: info.email,
-      password: md5(info.password),
-    });
-    if (rows.data.length > 0) {
-      var token = randomstring.generate(60);
-      await database.update("users", {
-        token: token
-      }, {
-        email: info.email
-      });
-      res.send({
-        status: 'success',
-        token: token
-      })
-    } else {
-      res.send({
-        status: 'error',
-        msg: 'password is not correct'
-      })
-    }
-  } else {
-    res.send({
-      status: 'error',
-      msg: 'email is not correct'
-    })
-  }
-});
+//   if (rows.data.length > 0) {
+//     rows = await database.select("users", {
+//       email: info.email,
+//       password: md5(info.password),
+//     });
+//     if (rows.data.length > 0) {
+//       var token = randomstring.generate(60);
+//       await database.update("users", {
+//         token: token
+//       }, {
+//         email: info.email
+//       });
+//       res.send({
+//         status: 'success',
+//         token: token
+//       })
+//     } else {
+//       res.send({
+//         status: 'error',
+//         msg: 'password is not correct'
+//       })
+//     }
+//   } else {
+//     res.send({
+//       status: 'error',
+//       msg: 'email is not correct'
+//     })
+//   }
+// });
 
-router.post("/register", async (req, res) => {
-  let data = req.body;
+// router.post("/register", async (req, res) => {
+//   let data = req.body;
 
-  let rows = await database.selectOr("users", {
-    userID: data.userID,
-    wallet: data.wallet,
-    email: data.email,
-  });
+//   let rows = await database.selectOr("users", {
+//     userID: data.userID,
+//     wallet: data.wallet,
+//     email: data.email,
+//   });
 
-  if (rows.status == "failed") {//database failed
-    res.send({ status: "failed", msg: "get: db error" });
-  } else {
-    if (rows.data.length > 0) {
-      res.send({
-        status: 'error',
-        msg: 'already registered'
-      })
-    } else {
-      rows = await database.select("transactions", {
-        sender: data.wallet,
-      });
+//   if (rows.status == "failed") {//database failed
+//     res.send({ status: "failed", msg: "get: db error" });
+//   } else {
+//     if (rows.data.length > 0) {
+//       res.send({
+//         status: 'error',
+//         msg: 'already registered'
+//       })
+//     } else {
+//       rows = await database.select("transactions", {
+//         sender: data.wallet,
+//       });
 
-      if (rows.status == "failed") {//database failed
-        res.send({ status: "failed", msg: "get: db error" });
-      } else {
-        var token_amount = 0;
-        rows.data.map(row => {
-          token_amount += row.amount * 1;
-        })
+//       if (rows.status == "failed") {//database failed
+//         res.send({ status: "failed", msg: "get: db error" });
+//       } else {
+//         var token_amount = 0;
+//         rows.data.map(row => {
+//           token_amount += row.amount * 1;
+//         })
 
-        var verify_code = randomstring.generate(20);
+//         var verify_code = randomstring.generate(20);
 
-        rows = await database.insert("users", {
-          ...data,
-          token_amount: token_amount,
-          password:md5(data.password),
-          verified:0,
-          verify_code:verify_code
-        });
+//         rows = await database.insert("users", {
+//           ...data,
+//           token_amount: token_amount,
+//           password:md5(data.password),
+//           verified:0,
+//           verify_code:verify_code
+//         });
         
-        const sendmail = require('sendmail')();
-        sendmail({
-            from: 'no-reply@yourdomain.com',
-            to: data.email,
-            subject: 'confirmable verify code',
-            html: '<h1>Confirm your email address</h1><p>Your confirmation code is below — enter it in your open browser window and we\'ll help you get signed in.</p><p>verify code:'+ verify_code + '</p>',
-          }, function(err, reply) {
-            console.log(err && err.stack);
-            console.dir(reply);
-        });
+//         const sendmail = require('sendmail')();
+//         sendmail({
+//             from: 'no-reply@yourdomain.com',
+//             to: data.email,
+//             subject: 'confirmable verify code',
+//             html: '<h1>Confirm your email address</h1><p>Your confirmation code is below — enter it in your open browser window and we\'ll help you get signed in.</p><p>verify code:'+ verify_code + '</p>',
+//           }, function(err, reply) {
+//             console.log(err && err.stack);
+//             console.dir(reply);
+//         });
 
-        rows = await database.select("users");
-        if (rows.status == "failed") {//database failed
-          res.send({ status: "failed", msg: "get list: db error" });
-        } else {
-          res.send(rows);
-        }
-      }
-    }
-  }
-});
+//         rows = await database.select("users");
+//         if (rows.status == "failed") {//database failed
+//           res.send({ status: "failed", msg: "get list: db error" });
+//         } else {
+//           res.send(rows);
+//         }
+//       }
+//     }
+//   }
+// });
 
 router.post("/exchange_score", async (req, res) => {
   let params = req.body;
