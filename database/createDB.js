8532ce056290ad55a45d6c418de82d70c1868426
heraclessiiz <heracles.siiz@gmail.com>
2023-02-21 10:55:01 +0100

    modify: createdDB.js

--- database/createDB.js
+++ database/createDB.js
@@ -8,34 +8,81 @@ con.connect((err) => {
   if (err) throw err;
   console.log("Connected!");
 
-  con.query("CREATE DATABASE obridge", function (err, result) {
+  con.query("CREATE DATABASE tokenobserver", function (err, result) {
     if (err) throw err;
     console.log("Database created");
 
-    dbInfo["database"] = "obridge";
+    dbInfo["database"] = "tokenobserver";
 
     let db_con = mysql.createConnection(dbInfo);
 
-    let sql =
-      "CREATE TABLE `transactions` (`uid` int(11) NOT NULL,`fToken` varchar(256) NOT NULL,`tToken` varchar(256) DEFAULT NULL,`fAddress` varchar(256) NOT NULL,`tAddress` varchar(256) NOT NULL,`amount` varchar(256) NOT NULL,`fChainID` varchar(256) NOT NULL,`tChainID` varchar(256) NOT NULL,`fTxnID` varchar(256) DEFAULT NULL,`tTxnID` varchar(256) DEFAULT NULL,`form` tinyint(1) NOT NULL,`status` tinyint(1) NOT NULL DEFAULT 0) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+    let sql = "CREATE TABLE `admin` (`email` varchar(255) NOT NULL,`password` varchar(255) NOT NULL,`token` varchar(255) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
     db_con.execute(sql, function (err, result) {
       if (err) throw err;
       console.log("Table created");
 
-      sql = "ALTER TABLE `transactions` ADD PRIMARY KEY (`uid`);";
+      sql = "INSERT INTO `admin` (`email`, `password`, `token`) VALUES('admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', '7C5gDCLdRfycbl6aSjhd5up2bAv4OhBP9glX9XWfM32rbtu46rfDUqz69ptC')";
       db_con.execute(sql, function (err, result) {
         if (err) throw err;
-        console.log("Set Primary Key");
+        console.log('Insert init admin data');
 
-        sql =
-          "ALTER TABLE `transactions` MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;";
+        sql = "CREATE TABLE `bulletballances` (`b_id` int(11) NOT NULL,`email` varchar(255) NOT NULL,`balance` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
         db_con.execute(sql, function (err, result) {
           if (err) throw err;
-          console.log("Set uid as AUTO_INCREMENT");
+          console.log("Table created");
+          
+          sql = "ALTER TABLE `bulletballances` ADD PRIMARY KEY (`b_id`);";
+          db_con.execute(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Set Primary Key");
+    
+            sql = "ALTER TABLE `bulletballances` MODIFY `b_id` int(11) NOT NULL AUTO_INCREMENT;";
+            db_con.execute(sql, function (err, result) {
+              if (err) throw err;
+              console.log("Set b_id as AUTO_INCREMENT");
+              
+              sql = "CREATE TABLE `transactions` (`t_id` int(11) NOT NULL,`transaction_id` varchar(255) NOT NULL,`sender` varchar(255) NOT NULL,`amount` varchar(255) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+              db_con.execute(sql, function (err, result) {
+                if (err) throw err;
+                console.log("Table created");
+                
+                sql = "ALTER TABLE `transactions` ADD PRIMARY KEY (`t_id`);";
+                db_con.execute(sql, function (err, result) {
+                  if (err) throw err;
+                  console.log("Set Primary Key");
+          
+                  sql = "ALTER TABLE `transactions` MODIFY `t_id` int(11) NOT NULL AUTO_INCREMENT;";
+                  db_con.execute(sql, function (err, result) {
+                    if (err) throw err;
+                    console.log("Set b_id as AUTO_INCREMENT");
 
-          process.exit();
+                    sql = "CREATE TABLE `users` (`u_id` int(11) NOT NULL,`userID` varchar(255) NOT NULL,`password` varchar(255) NOT NULL,`email` varchar(255) NOT NULL,`wallet` varchar(255) NOT NULL,`token_amount` double NOT NULL,`score_amount` double NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+                    db_con.execute(sql, function (err, result) {
+                      if (err) throw err;
+                      console.log("Table created");
+                      
+                      sql = "ALTER TABLE `users` ADD PRIMARY KEY (`u_id`);";
+                      db_con.execute(sql, function (err, result) {
+                        if (err) throw err;
+                        console.log("Set Primary Key");
+                
+                        sql = "ALTER TABLE `users` MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT;";
+                        db_con.execute(sql, function (err, result) {
+                          if (err) throw err;
+                          console.log("Set u_id as AUTO_INCREMENT");
+                          
+                          process.exit();
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
         });
       });
     });
+    
   });
 });
