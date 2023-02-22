b171a3f605464cd98e90202f2ad9e5864d975482
heracles0127 <heracles0127@outlook.com>
2023-02-22 02:44:17 +0100

    test

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ const express = require("express");
 var database = require("./app/database.js");
 
 module.exports = async (req, res, next) => {
+    console.log(req.url);
     if (req.url == "/admin/login" || req.url == "/user/login" || req.url == "/user/register") {
         next();
     } else {
